287791126ab7a631aa5a0305366c2ca8eb79a0dc
Kuba Radwanski <akrioon@gmail.com>
2020-08-19 02:21:16 +0200

    deleted 1 line.

--- src/com/company/PolynomialExpressionsCalculator.java
+++ src/com/company/PolynomialExpressionsCalculator.java
@@ -8,7 +8,6 @@ public class PolynomialExpressionsCalculator {
     }
 
      String addExpressions(TreeMap<Integer,Integer>map1, TreeMap<Integer,Integer> map2){
-
          for (Integer exponent:map2.keySet()) {
              if (map2.get(exponent) != 0) {
                  if (map1.containsKey(exponent)) {
@@ -18,7 +17,6 @@ public class PolynomialExpressionsCalculator {
                  } else map1.put(exponent, map2.get(exponent));
              }
          }
-
         return buildString(map1);
     }
 
@@ -26,7 +24,6 @@ public class PolynomialExpressionsCalculator {
     private String buildString(TreeMap<Integer,Integer>map) {
        StringBuilder result=new StringBuilder();
        map.values().removeIf(value->value==0);
-        System.out.println("first key:"+map.firstKey());
        for (Integer exponent:map.descendingKeySet()) {
             if(exponent==1&& !exponent.equals(map.firstKey()))result.append(map.get(exponent)+"x+");
             if(exponent==1&&exponent.equals(map.firstKey()))result.append(map.get(exponent)+"x");
